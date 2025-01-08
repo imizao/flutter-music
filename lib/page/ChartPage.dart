9f0859788e967c353089bbc4012966c005581087
imizao <2313466338@qq.com>
2025-01-08 19:44:40 +0800

    [Improved] Improved my page has added chart functionality

---
+++ lib/page/ChartPage.dart
@@ -0,0 +1,197 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class ChartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('图表')),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 3,
+          padding: EdgeInsets.all(16),
+          child: StockChart(),
+        ),
+      ),
+    );
+  }
+}
+
+class StockChart extends StatefulWidget {
+  @override
+  _StockChartState createState() => _StockChartState();
+}
+
+class _StockChartState extends State<StockChart> {
+  List<Map<String, dynamic>> stockData = [];
+  bool isLoading = true;
+  late ChartInfo chartInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchStockData();
+    fetchChartInfo();
+  }
+
+  Future<void> fetchStockData() async {
+    try {
+      final response = await http
+          .get(Uri.parse('http://localhost:8080/stockdata'))
+          .timeout(Duration(seconds: 10));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          stockData =
+              List<Map<String, dynamic>>.from(json.decode(response.body));
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load stock data');
+      }
+    } catch (e) {
+      setState(() => isLoading = false);
+      print('Error: $e');
+    }
+  }
+
+  Future<void> fetchChartInfo() async {
+    final response =
+        await http.get(Uri.parse('http://localhost:8080/chartinfo'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = json.decode(response.body);
+      setState(() {
+        chartInfo = ChartInfo.fromJson(jsonData);
+      });
+    } else {
+      throw Exception('Failed to load chart info');
+    }
+  }
+
+  Widget _buildChart() {
+    return SfCartesianChart(
+      primaryXAxis: CategoryAxis(
+        title: AxisTitle(text: '日期'),
+        labelRotation: -45,
+        majorGridLines: MajorGridLines(width: 0),
+      ),
+      primaryYAxis: NumericAxis(
+        title: AxisTitle(text: chartInfo.title),
+        majorGridLines: MajorGridLines(width: 1, color: Colors.grey[300]),
+      ),
+      plotAreaBorderWidth: 0,
+      annotations: <CartesianChartAnnotation>[
+        CartesianChartAnnotation(
+          widget: Container(
+            child: Text(
+              chartInfo.bullMarketLine,
+              style: TextStyle(color: Colors.green, fontSize: 12),
+            ),
+          ),
+          coordinateUnit: CoordinateUnit.point,
+          x: '2023-01-01',
+          y: 80,
+        ),
+        CartesianChartAnnotation(
+          widget: Container(
+            child: Text(
+              chartInfo.shockMarketLine,
+              style: TextStyle(color: Colors.orange, fontSize: 12),
+            ),
+          ),
+          coordinateUnit: CoordinateUnit.point,
+          x: '2023-01-01',
+          y: 60,
+        ),
+        CartesianChartAnnotation(
+          widget: Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.blue[50],
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Text(
+              chartInfo.description,
+              style: TextStyle(color: Colors.blue, fontSize: 12),
+            ),
+          ),
+          coordinateUnit: CoordinateUnit.point,
+          x: '2023-01-01',
+          y: 130,
+        ),
+      ],
+      series: [
+        LineSeries<Map<String, dynamic>, String>(
+          dataSource: stockData,
+          xValueMapper: (data, _) => data['date'].toString(),
+          yValueMapper: (data, _) => data['count'] as int,
+          dataLabelSettings: DataLabelSettings(
+            isVisible: true,
+            labelPosition: ChartDataLabelPosition.outside, // 显示在数据点上方
+            textStyle: TextStyle(
+              color: Colors.black, // 字体颜色
+              fontSize: 12, // 字体大小
+              fontWeight: FontWeight.bold, // 字体粗细
+            ),
+            builder: (dynamic data, dynamic point, dynamic series,
+                int pointIndex, int seriesIndex) {
+              return Text(
+                '${data['count']}', // 自定义标签内容
+                style: TextStyle(color: Colors.red, fontSize: 12),
+              );
+            },
+          ),
+          color: Colors.blue,
+          width: 2,
+          markerSettings: MarkerSettings(
+            isVisible: true,
+            shape: DataMarkerType.circle,
+            borderWidth: 2,
+            borderColor: Colors.blue,
+            color: Colors.white,
+          ),
+        ),
+      ],
+      tooltipBehavior: TooltipBehavior(
+        enable: true,
+        color: Colors.blue[800],
+        borderColor: Colors.blue[900],
+        textStyle: TextStyle(color: Colors.white, fontSize: 14),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (chartInfo == null) {
+      return Center(child: Text('数据加载失败'));
+    }
+    return isLoading
+        ? Center(child: CircularProgressIndicator())
+        : stockData.isEmpty
+            ? Center(child: Text('暂无数据'))
+            : _buildChart();
+  }
+}
+
+class ChartInfo {
+  final String title;
+  final String bullMarketLine;
+  final String shockMarketLine;
+  final String description;
+
+  ChartInfo(
+      this.title, this.bullMarketLine, this.shockMarketLine, this.description);
+
+  factory ChartInfo.fromJson(Map<String, dynamic> json) {
+    return ChartInfo(
+      json['title'],
+      json['bullMarketLine'],
+      json['shockMarketLine'],
+      json['description'],
+    );
+  }
+}
--- lib/page/ProfilePage.dart
+++ lib/page/ProfilePage.dart
@@ -1,8 +1,168 @@
 import 'package:flutter/material.dart';
+import 'ChartPage.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('我的内容'));
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('我的'),
+      // ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildHeader(),
+            _buildStats(),
+            _buildQuickActions(context),
+            _buildPlaylists(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundImage: NetworkImage(
+                'https://img2.baidu.com/it/u=1254672238,1423772385&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800'), // 替换为你的头像路径
+          ),
+          SizedBox(width: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('theboy',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Text('开启会员', style: TextStyle(fontSize: 14, color: Colors.grey)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStats() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildStatItem('关注', '216'),
+          _buildStatItem('粉丝', '117'),
+          _buildStatItem('金币', '51'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStatItem(String label, String value) {
+    return Column(
+      children: [
+        Text(value,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        Text(label, style: TextStyle(fontSize: 14, color: Colors.grey)),
+      ],
+    );
+  }
+
+  Widget _buildQuickActions(BuildContext context) {
+    // 接收 context
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildActionItem(context, Icons.bar_chart, '图表'), // 传递 context
+          _buildActionItem(context, Icons.brush, '装扮'),
+          _buildActionItem(context, Icons.calendar_today, '日签'),
+          _buildActionItem(context, Icons.collections, '收藏'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionItem(BuildContext context, IconData icon, String label) {
+    return GestureDetector(
+      onTap: () {
+        // 点击跳转到新页面
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ChartPage()), // 跳转到 ChartPage
+        );
+      },
+      child: Column(
+        children: [
+          Icon(icon, size: 30, color: Colors.blue), // 图标颜色设置为蓝色
+          SizedBox(height: 4),
+          Text(label, style: TextStyle(fontSize: 14, color: Colors.grey)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPlaylists() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('最近播放',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          SizedBox(height: 8),
+          _buildPlaylistItem('图表', '走势'),
+          _buildPlaylistItem('耳朵新区', '深度睡眠'),
+          _buildPlaylistItem('自建歌单', '20'),
+          _buildPlaylistItem('收藏歌单', '300'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPlaylistItem(String title, String subtitle) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4), // 增加垂直间距
+      decoration: BoxDecoration(
+        color: Colors.white, // 背景色
+        borderRadius: BorderRadius.circular(8), // 圆角
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2), // 阴影颜色
+            spreadRadius: 1,
+            blurRadius: 3,
+            offset: Offset(0, 2), // 阴影偏移
+          ),
+        ],
+      ),
+      child: ListTile(
+        contentPadding:
+            EdgeInsets.symmetric(horizontal: 16, vertical: 0), // 内边距
+        leading: Icon(Icons.music_note, color: Colors.blue), // 左侧图标
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.grey[600],
+          ),
+        ),
+        trailing:
+            Icon(Icons.arrow_forward_ios, size: 16, color: Colors.grey), // 右侧箭头
+        onTap: () {
+          // 点击事件
+          print('$title 被点击了');
+        },
+      ),
+    );
   }
 }
